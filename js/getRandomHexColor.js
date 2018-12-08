d0201b4bbcd0228f914e9f25dfbeb89f48f4cd0f
DimaGashko <dgashko@mail.ua>
2018-12-08 20:50:03 +0200

    getRandomColor

---
+++ js/getRandomHexColor.js
@@ -0,0 +1,7 @@
+/**
+ * Возвращает случайное число в формате HEX
+ */
+function getRandomHexColor() { 
+   const numColor = Math.round(Math.random() * 0xffffff);
+	return "#" + ("00000" + numColor.toString(16)).slice(-6);
+}
