a25e24eeaf3729f430aaa72e0684c01bad690fba
DimaGashko <dgashko@mail.ua>
2018-12-14 22:21:09 +0200

    getRandomHexColor

--- js/getRandomHexColor.js
+++ js/getRandomHexColor.js
@@ -3,5 +3,5 @@
  */
 function getRandomHexColor() { 
    const numColor = Math.round(Math.random() * 0xffffff);
-	return "#" + ("00000" + numColor.toString(16)).slice(-6);
+   return "#" + ("00000" + numColor.toString(16)).slice(-6);
 }
