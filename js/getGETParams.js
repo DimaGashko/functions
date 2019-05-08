4540baf5a5114b3b6d54f5c263fa6e2075f6f25d
Дмитрий Гашко <dgashko@mail.ua>
2019-05-08 16:43:58 +0300

    Update getGETParams.js

--- js/getGETParams.js
+++ js/getGETParams.js
@@ -1,21 +1,21 @@
 /**
- * Возвращает объект GET-параметров из строки адреса
+ * Returns the object of GET parameters
  * 
- * Возвращает объект вида:
+ * Return object format:
  * {
  *    promName: value,
  * }
  */
-function getGETParams() {
-   var search = location.search.slice(1);
-   var result = {};
+export default function getGETParams() {
+    var search = location.search.slice(1);
+    var result = {};
 
-   if (!search) return result;
+    if (!search) return result;
 
-   search.split('&').forEach(function (item) {
-      var component = item.split('=');
-      result[component[0]] = component[1];
-   });
+    search.split('&').forEach(function (item) {
+        var component = item.split('=', 2);
+        result[component[0]] = decodeURI(component[1]);
+    });
 
-   return result;
+    return result;
 }
